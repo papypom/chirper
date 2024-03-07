f94d7046bf35663692199e7b5aaa8702e16a3a01
ben <ben@saive.cc>
2024-03-07 23:22:35 +0100

    Trying stuff

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/posts/{id}', function($id) {
+    return response('<h1>Hello world : '.$id.'</h1>',200);
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
