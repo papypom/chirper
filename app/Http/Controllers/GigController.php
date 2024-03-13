8925c336bcf9cfd09e5b26e518a212d502a756ed
ben <ben@saive.cc>
2024-03-13 19:50:27 +0100

    Updating to 11 - before

---
+++ app/Http/Controllers/GigController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GigController extends Controller
+{
+    // Show all listings
+    public function index() {
+
+    }
+
+    public function show() 
+
+    //Show single listing 
+}
--- resources/views/components/gig-card.blade.php
+++ resources/views/components/gig-card.blade.php
@@ -8,24 +8,7 @@
                 <a href="/gig/{{ $gig->id }}">{{ $gig->title }}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{ $gig->company }}</div>
-            <ul class="flex">
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Laravel</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">API</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Backend</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Vue</a>
-                </li>
-            </ul>
+            <x-gig-tags :tagsCsv="$gig->tags" class="text-xs"/>
             <div class="text-lg mt-4">
                 <i class="fa-solid fa-location-dot"></i> {{ $gig->location }}
             </div>
